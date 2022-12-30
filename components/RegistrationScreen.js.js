14d9d94a78a1fbd700390a94252e3bb4ea30abf0
CarlMenke <carlmenke13@gmail.com>
2022-12-30 13:43:18 -0800

    added the imports for storing data with firebase

--- components/RegistrationScreen.js.js
+++ components/RegistrationScreen.js.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { setIsSignedIn } from '../redux/actions';
 import { firebase } from '../firebaseConfig'
+import { getApps, initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export default function RegistrationScreen() {
 
@@ -10,6 +12,7 @@ export default function RegistrationScreen() {
     const setIsSignedInAction = () => dispatch(setIsSignedIn(true))
 
     //YOU SHOULD BE ABLE TO WRITE TO A FIREBASE DATABASE FROM HERE
+
     //maybe be able to watch a firebase documents? subscriptions?
     //first be able to read and write from a database
     //second anuthenticate all the calls to and from the database  (research the professional ways that acceptable with apple)
