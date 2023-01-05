b00e4dde99a04188a87f58430ea78bd098851eb2
carlmenke <carlmenke13@gmail.com>
2023-01-05 14:10:45 -0800

    added firebase wirting aility

--- components/RegistrationScreen.js.js
+++ components/RegistrationScreen.js.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { setIsSignedIn } from '../redux/actions';
-import { firebase } from '../firebaseConfig'
+import { firebaseConfig } from '../firebaseConfig'
 import { getApps, initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
@@ -11,6 +11,18 @@ export default function RegistrationScreen() {
 
     const setIsSignedInAction = () => dispatch(setIsSignedIn(true))
 
+    const [userName, setUserName] = useState()
+    const [password, setPassword] = useState()
+
+    const signInAttempt = async () => {
+      const data = {
+        userName: userName,
+        password: password
+      }
+      const res = await firebaseConfig.collection('users').doc('test2').set(data)
+      console.log(res)
+    }
+ 
     //YOU SHOULD BE ABLE TO WRITE TO A FIREBASE DATABASE FROM HERE
 
     //maybe be able to watch a firebase documents? subscriptions?
@@ -29,9 +41,20 @@ export default function RegistrationScreen() {
     return (
         <View style={styles.container}>
             <Text>Login Or Signup</Text>
+            <TextInput
+              onChangeText = {setUserName}
+              value = {userName}
+              placeholder = "Enter Username"/>
+            <TextInput 
+              onChangeText = {setPassword}
+              value = {password}
+              placeholder = "Enter Password"/>
             <Button
                 onPress = {setIsSignedInAction}
-                title = "Enter" />
+                title = "Go to signed in screen" />
+            <Button
+                onPress = {signInAttempt}
+                title = "Add user to firebase" />
         </View>
     )
 }
